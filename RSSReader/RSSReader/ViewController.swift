a1c9fefaa46ed7cf8bcb15cee0dceb04881c8f8c
Eugeniy Popov <Eugeny@MacBook-Pro-Eugeniy.local>
2016-01-09 18:59:13 +0300

    init

---
+++ RSSReader/RSSReader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RSSReader
+//
+//  Created by Eugeniy Popov on 09.01.16.
+//  Copyright © 2016 Eugeniy Popov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
